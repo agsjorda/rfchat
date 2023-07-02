d7882f715093115af334f8954ece7b9e5b8afee1
agsjorda <agsjorda@gmail.com>
2023-07-02 13:50:42 +0800

    edited register and login link color

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,9 +22,14 @@ const Login = () => {
 	return (
 		<div className="flex items-center justify-center h-screen bg-[#0081a7]">
 			<div className="flex flex-col items-center px-20 py-16 bg-[#f1faee] rounded-md ">
-				<span className="font-bold text-[#0081a7] text-3xl">RF Chat</span>
+				<span className="font-bold text-[#0081a7] text-3xl">
+					RF Chat
+				</span>
 				<span>Log In</span>
-				<form className="flex flex-col gap-y-2 items-center justify-center w-[300px]" onSubmit={handleSubmit}>
+				<form
+					className="flex flex-col gap-y-2 items-center justify-center w-[300px]"
+					onSubmit={handleSubmit}
+				>
 					<input
 						className="border-b-[1px] border-solid border-slate-400 bg-[#f1faee] pl-3 w-full"
 						name="email"
@@ -38,11 +43,16 @@ const Login = () => {
 						placeholder="password"
 					/>
 
-					<button className="bg-[#0081a7] text-white w-full h-8 rounded-md my-2">Sign In</button>
+					<button className="bg-[#0081a7] text-white w-full h-8 rounded-md my-2">
+						Sign In
+					</button>
 				</form>
 				{err && <span>Something went wrong</span>}
 				<p>
-					Don&apos;t have an account? <Link to="/register">Register</Link>
+					Don&apos;t have an account?{" "}
+					<Link to="/register" className="text-[#0081a7]">
+						Register
+					</Link>
 				</p>
 			</div>
 		</div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -18,7 +18,11 @@ const Register = () => {
 		const file = e.target.file.files[0];
 
 		try {
-			const res = await createUserWithEmailAndPassword(auth, email, password);
+			const res = await createUserWithEmailAndPassword(
+				auth,
+				email,
+				password
+			);
 			console.log(res);
 			const storageRef = ref(storage, displayName);
 
@@ -29,7 +33,8 @@ const Register = () => {
 				(snapshot) => {
 					// Observe state change events such as progress, pause, and resume
 					// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-					const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+					const progress =
+						(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
 					console.log("Upload is " + progress + "% done");
 					switch (snapshot.state) {
 						case "paused":
@@ -46,20 +51,25 @@ const Register = () => {
 					setErr(true);
 				},
 				() => {
-					getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-						await updateProfile(res.user, {
-							displayName,
-							photoURL: downloadURL,
-						});
-						await setDoc(doc(db, "users", res.user.uid), {
-							uid: res.user.uid,
-							displayName,
-							email,
-							photoURL: downloadURL,
-						});
-						await setDoc(doc(db, "userChats", res.user.uid), {});
-						navigate("/");
-					});
+					getDownloadURL(uploadTask.snapshot.ref).then(
+						async (downloadURL) => {
+							await updateProfile(res.user, {
+								displayName,
+								photoURL: downloadURL,
+							});
+							await setDoc(doc(db, "users", res.user.uid), {
+								uid: res.user.uid,
+								displayName,
+								email,
+								photoURL: downloadURL,
+							});
+							await setDoc(
+								doc(db, "userChats", res.user.uid),
+								{}
+							);
+							navigate("/");
+						}
+					);
 				}
 			);
 		} catch (err) {
@@ -70,9 +80,14 @@ const Register = () => {
 	return (
 		<div className="flex items-center justify-center h-screen bg-[#0081a7]">
 			<div className="flex flex-col items-center px-20 py-16 bg-[#f1faee] rounded-md ">
-				<span className="font-bold text-[#0081a7] text-3xl">RF Chat</span>
+				<span className="font-bold text-[#0081a7] text-3xl">
+					RF Chat
+				</span>
 				<span>Register</span>
-				<form onSubmit={handleSubmit} className="flex flex-col gap-y-2 items-center justify-center w-[300px]">
+				<form
+					onSubmit={handleSubmit}
+					className="flex flex-col gap-y-2 items-center justify-center w-[300px]"
+				>
 					<input
 						className="border-b-[1px] border-solid border-slate-400 bg-[#f1faee] pl-3 w-full"
 						name="displayName"
@@ -91,18 +106,32 @@ const Register = () => {
 						type="password"
 						placeholder="password"
 					/>
-					<input style={{ display: "none" }} className="flex" name="file" type="file" id="file" />
+					<input
+						style={{ display: "none" }}
+						className="flex"
+						name="file"
+						type="file"
+						id="file"
+					/>
 					<label className="flex items-center gap-8" htmlFor="file">
 						<img className="w-8" src={AddImage} alt="" />
-						<span className="text-[#69858d] cursor-pointer text-sm">add an avatar</span>
+						<span className="text-[#69858d] cursor-pointer text-sm">
+							add an avatar
+						</span>
 					</label>
-					<button type="submit" className="bg-[#0081a7] text-white w-full h-8 rounded-md my-2">
+					<button
+						type="submit"
+						className="bg-[#0081a7] text-white w-full h-8 rounded-md my-2"
+					>
 						Sign up
 					</button>
 					{err && <span>Something went wrong</span>}
 				</form>
 				<p>
-					Already have an account? <Link to="/login">Login</Link>
+					Already have an account?{" "}
+					<Link to="/login" className="text-[#0081a7]">
+						Login
+					</Link>
 				</p>
 			</div>
 		</div>
